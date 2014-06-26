5300a8f8c8e5a9a3cfad0eedff559e27a80478b8
sdbaobaodww <duanwwu@DuanwwudeiMac.local>
2014-06-26 09:58:22 +0800

    调整迁移过来的代码

---
+++ DZHDrawable/Addition/UIColor+RGB.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+RGB.h
+//  DZHDrawable
+//
+//  Created by Duanwwu on 14-6-26.
+//  Copyright (c) 2014年 Duanwwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (RGB)
+
++ (UIColor *)colorFromRGB:(int)rgbValue;
+
+@end
---
+++ DZHDrawable/Addition/UIColor+RGB.m
@@ -0,0 +1,21 @@
+//
+//  UIColor+RGB.m
+//  DZHDrawable
+//
+//  Created by Duanwwu on 14-6-26.
+//  Copyright (c) 2014年 Duanwwu. All rights reserved.
+//
+
+#import "UIColor+RGB.h"
+
+@implementation UIColor (RGB)
+
++ (UIColor *)colorFromRGB:(int)rgbValue
+{
+    return [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0
+                           green:((float)((rgbValue & 0xFF00) >> 8))/255.0
+                            blue:((float)(rgbValue & 0xFF))/255.0
+                           alpha:1.0];
+}
+
+@end
